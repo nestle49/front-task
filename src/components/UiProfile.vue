7b2a22cfa7db6af218f1df19c549c62579831cd9
Albert <nestle49@ya.ru>
2025-10-31 01:31:23 +0200

    feat: styled component

--- src/components/UiProfile.vue
+++ src/components/UiProfile.vue
@@ -35,10 +35,10 @@
            height="80"
            loading="lazy"
            alt="Student photo"
-      /><!-- TODO: with api add skeleton -->
+      /><!-- TODO: skeleton -->
     </div>
 
-    <div class="flex flex-col gap-3">
+    <div class="flex flex-col gap-3 flex-1 min-w-0">
       <label
         for="hours-old"
         class="font-koulen text-base leading-[15px] tracking-[0.02em] ease transition-colors duration-200"
@@ -50,17 +50,32 @@
       </label>
 
       <div class="flex gap-3 items-center">
-        <input
-          ref="input"
-          name="hours-old"
-          id="hours-old"
-          class="border-b-gray-300 border-b w-[50px]"
-          :class="{
-            focused
-          }"
-          type="number"
-        />
-        <div class="text font-inter not-italic font-normal text-[18px] leading-[22px] text-[color:var(--ui-dark)]">hours old</div>
+        <!-- зеркальный span -->
+        <div class="relative overflow-hidden">
+          <span
+            ref="mirror"
+            class="absolute invisible whitespace-pre font-inter text-[18px] leading-[22px] max-w-full"
+          >
+            {{ value }}
+          </span>
+
+          <input
+            ref="input"
+            type="text"
+            name="hours-old"
+            id="hours-old"
+            class="duration-200 transition-border-color transition-colors transition-opacity outline-none w-[50px] py-[10px] px-[8px] font-inter font-medium text-[18px] leading-[22px] border-solid rounded-[6px] truncate max-w-[calc(100%-1px)]"
+            :class="[
+              focused ? 'border-[1.5px] border-[color:var(--ui-primary-light)] text-[color:var(--ui-dark)]' : 'border-[1px] border-[color:var(--ui-light-gray)] text-[color:var(--ui-dark)]/30'
+            ]"
+            :style="{ width: `${inputWidth}px` }"
+            v-model="value"
+          />
+        </div>
+        <div
+          class="text font-inter not-italic font-normal whitespace-nowrap text-[18px] leading-[22px] text-[color:var(--ui-dark)]">
+          hours old
+        </div>
       </div>
     </div>
   </div>
@@ -76,4 +91,10 @@ defineProps<{
 const input = useTemplateRef<HTMLInputElement>('input')
 const { focused } = useFocus(input, { initialValue: false })
 onStartTyping(() => input.value?.focus())
+
+const value = ref('')
+const inputWidth = computed(() => Math.min(Math.max(width.value + 22, 72), 400)) // padding + границы
+
+const mirror = useTemplateRef<HTMLSpanElement>('mirror')
+const { width } = useElementSize(mirror)
 </script>
